74ebb92e55c0045b78c50c8a976875323d10559e
James <beardyjames@live.co.uk>
2013-09-28 15:03:47 +0100

    Tweak annotation to enforce @Target and @Retention

--- src/main/java/me/tehbeard/BeardAch/dataSource/json/help/ComponentValueDescription.java
+++ src/main/java/me/tehbeard/BeardAch/dataSource/json/help/ComponentValueDescription.java
@@ -1,5 +1,12 @@
 package me.tehbeard.BeardAch.dataSource.json.help;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(value=ElementType.FIELD)
+@Retention(RetentionPolicy.SOURCE)
 public @interface ComponentValueDescription {
 String description();
 String[] examples() default {};
